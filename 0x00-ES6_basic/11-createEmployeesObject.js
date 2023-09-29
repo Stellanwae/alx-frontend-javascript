6bde0f65706fdecfe0808f2f26df546ce2bbcf00
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2023-09-29 15:25:03 +0300

    Update 11-createEmployeesObject.js

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,3 @@
 export default function createEmployeesObject(departmentName, employees) {
-  const employeeInfo = {
-    [departmentName]: [...employees]
-  };
-  return employeeInfo;
+  return { [departmentName]: [...employees] };
 }
