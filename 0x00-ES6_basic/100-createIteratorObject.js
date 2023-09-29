727b9504efe39ff1a8477238e4289f4304c5b1c6
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2023-09-29 16:15:59 +0300

    Create 100-createIteratorObject.js

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,10 @@
+export default function createIteratorObject(report) {
+  let allEmps = [];
+  for (const item of Object.values(report.allEmps)) {
+    allEmps = [
+      ...allEmps,
+      ...item,
+    ];
+  }
+  return allEmps;
+}
